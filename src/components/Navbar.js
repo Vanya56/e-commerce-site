3599b3538895087375ce235bc3eadc927f7e91f6
Vanya Levchenko <ivanlevchenko56@gmail.com>
2020-02-17 01:05:13 -0700

    Navbar and button styled component added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
 import logo from '../logo.png';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
             <Link Link to="/">
-            <img src={logo} alt="store" className="navbar-brand" />
+            <img src={logo} alt="store" className="navbar-brand" height="100px" width="100px" />
             </Link>
-            </nav>
+            <ul className="navbar-nav align-items-center">
+            <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+            Products
+            </Link>
+            </li>
+            </ul>
+            <Link to="/cart" className="ml-auto">
+            <ButtonContainer>
+                <span className="mr-2">
+                <i className="fa fa-shopping-cart" />
+                </span>
+                My Cart
+            </ButtonContainer>
+            </Link>
+            </NavWrapper>
         )
     }
 }
+
+const NavWrapper = styled.nav `
+    background: var(--mainBlue);
+    .nav-link {
+        color: var(--mainWhite);
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
+
+`
