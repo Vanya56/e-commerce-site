2fc03af12052df8906f3d23989f860331f2a8a0e
Vanya Levchenko <ivanlevchenko56@gmail.com>
2020-02-24 23:31:29 -0700

    Rendered images and title to details page.

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,45 @@
 import React, {Component} from 'react'
+import {ProductConsumer} from '../context';
+import {Link} from 'react-router-dom';
+import {buttonContainer} from './Button';
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Details!</h3>
-            </div>
-        )
+            <ProductConsumer>
+                {(value) => {
+                    const {
+                        id,
+                        company,
+                        img,
+                        info,
+                        price,
+                        title,
+                        inCart
+                    } = value.detailProduct;
+                    return (
+                        <div className="container py-5">
+                            {/* Title */}
+                            <div className="row">
+                                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                                    <h1>{title}</h1>
+                                </div>
+                            </div>
+                            {/* End title */}
+                            {/* Product info */}
+                            <div className="row">
+                                <div className="col-10 mx-auto col-md-6 my-3">
+                                    <img src={img} className="img-fluid" alt="product"/>
+                                </div>
+                            
+                            {/* product text */}
+                            <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                            </div>
+                            </div>
+                        </div>
+                    );
+                }}
+            </ProductConsumer>
+        );
     }
 }
