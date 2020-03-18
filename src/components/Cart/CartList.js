8f75130a4122e6be85f802a5474afe2764a206e2
Vanya Levchenko <ivanlevchenko56@gmail.com>
2020-03-17 20:27:23 -0600

    Image, product name, price, and decrement button added to cart page.

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -2,13 +2,14 @@ import React from 'react'
 import CartItem from './CartItem';
 
 export default function CartList({value}) {
-    const cart = value;
-    console.log(value, cart);
-    
+    const {cart} = value;
+
     return (
-        <div>
-            <h3>Hello from CartList</h3>
-            <CartItem />
+        <div className="container-fulid">
+            {cart.map(item => {
+                return <CartItem key={item.id} item={item} value={value}/>
+            })}
+
         </div>
     )
 }
