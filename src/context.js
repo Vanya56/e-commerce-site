f0d2048b5415622c79b0d5948af1fbc828d30951
Vanya Levchenko <ivanlevchenko56@gmail.com>
2020-03-23 17:34:42 -0600

    Increment functionality added to cart page.

--- src/context.js
+++ src/context.js
@@ -79,7 +79,16 @@ class ProductProvider extends Component {
         })
     };
     increment = (id) => {
-        console.log("This is the increment method.");
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id)
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+
+        this.setState(()=>{return{cart:[...tempCart]}},()=>{this.addTotals()})
 
     };
     decrement = (id) => {
